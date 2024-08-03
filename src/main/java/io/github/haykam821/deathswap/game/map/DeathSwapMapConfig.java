01ec89b75055a3e38d74160a9b7461dfc0e5e4ae
uberswe <m@rkus.io>
2024-08-03 16:55:17 +0200

    Made deathswap a Create addon and added a bunch of Create items

--- src/main/java/io/github/haykam821/deathswap/game/map/DeathSwapMapConfig.java
+++ src/main/java/io/github/haykam821/deathswap/game/map/DeathSwapMapConfig.java
@@ -1,41 +1,52 @@
 package io.github.haykam821.deathswap.game.map;
 
+import java.util.Optional;
+
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
 import io.github.haykam821.deathswap.Main;
 import net.minecraft.block.Blocks;
+import net.minecraft.registry.RegistryCodecs;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.entry.RegistryEntryList;
 import net.minecraft.registry.entry.RegistryFixedCodec;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.dimension.DimensionOptions;
 import net.minecraft.world.dimension.DimensionOptionsRegistryHolder;
 import net.minecraft.world.gen.WorldPreset;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 
 public class DeathSwapMapConfig {
 	public static final Codec<DeathSwapMapConfig> CODEC = RecordCodecBuilder.create(instance -> {
 		return instance.group(
-			RegistryFixedCodec.of(RegistryKeys.WORLD_PRESET).fieldOf("preset").forGetter(mapConfig -> mapConfig.worldPreset),
-			RegistryKey.createCodec(RegistryKeys.DIMENSION).optionalFieldOf("dimension_options", DimensionOptions.OVERWORLD).forGetter(mapConfig -> mapConfig.dimensionOptions),
-			BlockStateProvider.TYPE_CODEC.optionalFieldOf("barrier", BlockStateProvider.of(Blocks.BARRIER)).forGetter(DeathSwapMapConfig::getBarrier),
-			BlockStateProvider.TYPE_CODEC.optionalFieldOf("top_barrier", BlockStateProvider.of(Main.BARRIER_AIR)).forGetter(DeathSwapMapConfig::getTopBarrier),
-			Codec.INT.optionalFieldOf("x", 32).forGetter(DeathSwapMapConfig::getX),
-			Codec.INT.optionalFieldOf("z", 32).forGetter(DeathSwapMapConfig::getZ)
+				RegistryFixedCodec.of(RegistryKeys.WORLD_PRESET).fieldOf("preset").forGetter(mapConfig -> mapConfig.worldPreset),
+				RegistryKey.createCodec(RegistryKeys.DIMENSION).optionalFieldOf("dimension_options", DimensionOptions.OVERWORLD).forGetter(mapConfig -> mapConfig.dimensionOptions),
+				RegistryCodecs.entryList(RegistryKeys.BIOME).optionalFieldOf("excluded_biomes").forGetter(mapConfig -> mapConfig.excludedBiomes),
+				BlockStateProvider.TYPE_CODEC.optionalFieldOf("barrier", BlockStateProvider.of(Blocks.BARRIER)).forGetter(DeathSwapMapConfig::getBarrier),
+				BlockStateProvider.TYPE_CODEC.optionalFieldOf("top_barrier", BlockStateProvider.of(Main.BARRIER_AIR)).forGetter(DeathSwapMapConfig::getTopBarrier),
+				Codec.INT.optionalFieldOf("x", 32).forGetter(DeathSwapMapConfig::getX),
+				Codec.INT.optionalFieldOf("z", 32).forGetter(DeathSwapMapConfig::getZ)
 		).apply(instance, DeathSwapMapConfig::new);
 	});
 
 	private final RegistryEntry<WorldPreset> worldPreset;
 	private final RegistryKey<DimensionOptions> dimensionOptions;
+	private final Optional<RegistryEntryList<Biome>> excludedBiomes;
 	private final BlockStateProvider barrier;
 	private final BlockStateProvider topBarrier;
 	private final int x;
 	private final int z;
 
-	public DeathSwapMapConfig(RegistryEntry<WorldPreset> worldPreset, RegistryKey<DimensionOptions> dimensionOptions, BlockStateProvider barrier, BlockStateProvider topBarrier, int x, int z) {
+	private ChunkGenerator chunkGenerator;
+
+	public DeathSwapMapConfig(RegistryEntry<WorldPreset> worldPreset, RegistryKey<DimensionOptions> dimensionOptions, Optional<RegistryEntryList<Biome>> excludedBiomes, BlockStateProvider barrier, BlockStateProvider topBarrier, int x, int z) {
 		this.worldPreset = worldPreset;
 		this.dimensionOptions = dimensionOptions;
+		this.excludedBiomes = excludedBiomes;
 		this.barrier = barrier;
 		this.topBarrier = topBarrier;
 		this.x = x;
--- src/main/java/io/github/haykam821/deathswap/game/phase/DeathSwapActivePhase.java
+++ src/main/java/io/github/haykam821/deathswap/game/phase/DeathSwapActivePhase.java
@@ -5,12 +5,18 @@ import java.util.Set;
 
 import com.google.common.collect.Sets;
 
+import com.simibubi.create.AllBlocks;
+import com.simibubi.create.AllItems;
+import com.simibubi.create.Create;
 import io.github.haykam821.deathswap.game.DeathSwapConfig;
 import io.github.haykam821.deathswap.game.DeathSwapTimer;
 import io.github.haykam821.deathswap.game.EliminationCollector;
 import io.github.haykam821.deathswap.game.map.DeathSwapMap;
 import io.github.haykam821.deathswap.game.map.DeathSwapMapConfig;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.MutableText;
@@ -41,6 +47,8 @@ public class DeathSwapActivePhase implements GameActivityEvents.Enable, GameActi
 	private final EliminationCollector eliminationCollector = new EliminationCollector(this);
 	private boolean singleplayer;
 
+	private int ticksUntilClose = -1;
+
 	public DeathSwapActivePhase(GameSpace gameSpace, ServerWorld world, GlobalWidgets widgets, DeathSwapMap map, DeathSwapConfig config, Set<ServerPlayerEntity> players) {
 		this.gameSpace = gameSpace;
 		this.world = world;
@@ -59,7 +67,7 @@ public class DeathSwapActivePhase implements GameActivityEvents.Enable, GameActi
 			// Rules
 			activity.allow(GameRuleType.BLOCK_DROPS);
 			activity.allow(GameRuleType.CRAFTING);
-			activity.allow(GameRuleType.FALL_DAMAGE);
+			activity.deny(GameRuleType.FALL_DAMAGE);
 			activity.allow(GameRuleType.HUNGER);
 			activity.deny(GameRuleType.PORTALS);
 			activity.deny(GameRuleType.PVP);
@@ -70,6 +78,41 @@ public class DeathSwapActivePhase implements GameActivityEvents.Enable, GameActi
 			activity.listen(GamePlayerEvents.OFFER, phase);
 			activity.listen(PlayerDeathEvent.EVENT, phase);
 			activity.listen(GamePlayerEvents.REMOVE, phase);
+			//LOGGER.info("Create addon mod [{}] is loading alongside Create [{}]!", NAME, Create.VERSION);
+
+			for (ServerPlayerEntity p : players) {
+				p.getInventory().setStack(0, new ItemStack(AllItems.GOGGLES));
+				p.getInventory().setStack(1, new ItemStack(AllItems.WRENCH));
+				p.getInventory().setStack(2, new ItemStack(Items.IRON_PICKAXE));
+				p.getInventory().setStack(3, new ItemStack(Items.IRON_AXE));
+				p.getInventory().setStack(4, new ItemStack(Items.WATER_BUCKET));
+				p.getInventory().setStack(5, new ItemStack(Items.CRAFTING_TABLE));
+				p.getInventory().setStack(6, new ItemStack(AllItems.EXTENDO_GRIP));
+				p.getInventory().setStack(7, new ItemStack(AllItems.COPPER_BACKTANK));
+				p.getInventory().setStack(9, new ItemStack(AllItems.SUPER_GLUE));
+				p.getInventory().setStack(10, new ItemStack(AllBlocks.LARGE_WATER_WHEEL, 8));
+				p.getInventory().setStack(11, new ItemStack(AllBlocks.BELT, 64));
+				p.getInventory().setStack(12, new ItemStack(AllBlocks.SHAFT, 64));
+				p.getInventory().setStack(13, new ItemStack(AllBlocks.MECHANICAL_BEARING, 64));
+				p.getInventory().setStack(14, new ItemStack(AllBlocks.LARGE_COGWHEEL, 64));
+				p.getInventory().setStack(15, new ItemStack(AllBlocks.COGWHEEL, 64));
+				p.getInventory().setStack(16, new ItemStack(AllBlocks.MECHANICAL_SAW, 64));
+				p.getInventory().setStack(17, new ItemStack(AllBlocks.MECHANICAL_DRILL, 64));
+				p.getInventory().setStack(18, new ItemStack(AllBlocks.ENCASED_FAN, 64));
+				p.getInventory().setStack(19, new ItemStack(AllBlocks.ENCASED_CHAIN_DRIVE, 64));
+				p.getInventory().setStack(20, new ItemStack(AllBlocks.ANDESITE_SCAFFOLD, 64));
+				p.getInventory().setStack(21, new ItemStack(Items.OAK_PLANKS, 64));
+				p.getInventory().setStack(22, new ItemStack(AllBlocks.ANDESITE_CASING, 64));
+				p.getInventory().setStack(23, new ItemStack(AllBlocks.BLAZE_BURNER, 64));
+				p.getInventory().setStack(24, new ItemStack(Items.COAL, 64));
+				p.getInventory().setStack(25, new ItemStack(AllBlocks.MECHANICAL_PISTON, 64));
+				p.getInventory().setStack(26, new ItemStack(AllBlocks.MECHANICAL_ROLLER, 64));
+				p.getInventory().setStack(27, new ItemStack(AllBlocks.PISTON_EXTENSION_POLE, 64));
+				p.getInventory().setStack(28, new ItemStack(AllBlocks.STICKY_MECHANICAL_PISTON, 64));
+				p.getInventory().setStack(29, new ItemStack(AllBlocks.GEARBOX, 64));
+				p.getInventory().setStack(30, new ItemStack(AllBlocks.ROTATION_SPEED_CONTROLLER, 64));
+				p.getInventory().setStack(31, new ItemStack(Items.IRON_INGOT, 64));
+			}
 		});
 	}
 
@@ -89,6 +132,19 @@ public class DeathSwapActivePhase implements GameActivityEvents.Enable, GameActi
 		this.timer.tick();
 		this.eliminationCollector.tick();
 
+		// Decrease ticks until game end to zero
+		if (this.isGameEnding()) {
+			if (this.ticksUntilClose == 0) {
+				this.gameSpace.close(GameCloseReason.FINISHED);
+			}
+
+			this.ticksUntilClose -= 1;
+			this.eliminationCollector.tick();
+
+			return;
+		}
+
+
 		// Eliminate players that are out of bounds
 		Iterator<ServerPlayerEntity> iterator = this.players.iterator();
 		while (iterator.hasNext()) {
@@ -105,7 +161,7 @@ public class DeathSwapActivePhase implements GameActivityEvents.Enable, GameActi
 			if (this.players.size() == 1 && this.singleplayer) return;
 
 			this.gameSpace.getPlayers().sendMessage(this.getEndingMessage().formatted(Formatting.GOLD));
-			this.gameSpace.close(GameCloseReason.FINISHED);
+			this.ticksUntilClose = this.config.getTicksUntilClose().get(this.world.getRandom());
 		}
 	}
 
@@ -124,6 +180,11 @@ public class DeathSwapActivePhase implements GameActivityEvents.Enable, GameActi
 
 	@Override
 	public ActionResult onDeath(ServerPlayerEntity player, DamageSource source) {
+		if (this.isGameEnding()) {
+			DeathSwapActivePhase.spawn(this.world, this.map, this.config.getMapConfig(), player);
+			return ActionResult.FAIL;
+		}
+
 		if (this.players.contains(player) && this.world.getGameRules().getBoolean(GameRules.SHOW_DEATH_MESSAGES)) {
 			Text message = player.getDamageTracker().getDeathMessage().copy().formatted(Formatting.RED);
 			this.gameSpace.getPlayers().sendMessage(message);
@@ -182,6 +243,9 @@ public class DeathSwapActivePhase implements GameActivityEvents.Enable, GameActi
 	}
 
 	private void sendEliminateMessage(ServerPlayerEntity player, String suffix) {
+		if (this.isGameEnding()) {
+			return;
+		}
 		Text message = Text.translatable("text.deathswap.eliminated" + suffix, player.getDisplayName()).formatted(Formatting.RED);
 		this.gameSpace.getPlayers().sendMessage(message);
 	}
@@ -192,6 +256,10 @@ public class DeathSwapActivePhase implements GameActivityEvents.Enable, GameActi
 
 	public static float getSpawnYaw(ServerWorld world) {
 		return world.getRandom().nextInt(3) * 90;
+	}
+
+	public boolean isGameEnding() {
+		return this.ticksUntilClose >= 0;
 	}
 
 	public static void spawn(ServerWorld world, DeathSwapMap map, DeathSwapMapConfig mapConfig, ServerPlayerEntity player) {
